8e5d503e756b8c717c65111709ac0ddd9bc627a3
Ethan Bennett <746393443@qq.com>
2024-12-16 12:05:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.159.1.66/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/10.159.1.66/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
